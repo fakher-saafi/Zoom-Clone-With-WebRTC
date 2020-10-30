6d6f64dc9f35f230f5f07e9760d412640600fc88
fakher <fakher.saafi@esprit.tn>
2020-10-30 19:01:41 +0100

    start

--- public/script.js
+++ public/script.js
@@ -41,7 +41,7 @@ function connectToNewUser(userId, stream) {
   const call = myPeer.call(userId, stream)
   const video = document.createElement('video')
   call.on('stream', userVideoStream => {
-    if (device=="desktop")
+    //if (device=="desktop")
     addVideoStream(video, userVideoStream)
   })
   call.on('close', () => {
