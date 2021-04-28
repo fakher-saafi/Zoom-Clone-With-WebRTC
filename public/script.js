4bc5f58d1b5d36f4435dde61a27e72ada6ed929a
fakher-saafi <fakher.saafi@esprit.tn>
2021-04-28 03:27:15 +0100

    Update script.js
    Desk

--- public/script.js
+++ public/script.js
@@ -60,16 +60,16 @@ function addVideoStream(video, stream) {
 }
 
 function getDeviceType() {
-  const ua = navigator.userAgent;
-  if (/(tablet|ipad|playbook|silk)|(android(?!.*mobi))/i.test(ua)) {
-    return "tablet";
-  }
-  if (
-      /Mobile|iP(hone|od|ad)|Android|BlackBerry|IEMobile|Kindle|Silk-Accelerated|(hpw|web)OS|Opera M(obi|ini)/.test(
-          ua
-      )
-  ) {
-    return "mobile";
-  }
+ // const ua = navigator.userAgent;
+//  if (/(tablet|ipad|playbook|silk)|(android(?!.*mobi))/i.test(ua)) {
+ //   return "tablet";
+//  }
+//  if (
+//      /Mobile|iP(hone|od|ad)|Android|BlackBerry|IEMobile|Kindle|Silk-Accelerated|(hpw|web)OS|Opera M(obi|ini)/.test(
+ //         ua
+ //     )
+ // ) {
+//    return "mobile";
+//  }
   return "desktop";
 };
